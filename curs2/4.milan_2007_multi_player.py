8f090d3f506572bb97b2cb82042e05e5fba556fd
Laura Popescu <laura.liliana.popescu@gmail.com>
2025-05-14 21:03:24 +0300

    ploturi si aplicatii in Django

---
+++ curs2/4.milan_2007_multi_player.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+folder_name = "Milan2007"
+
+first_image = folder_name + "/"+"0.Kaka.png"
+
+figure, axis = plt.subplots(nrows = 1, ncols = 2)
+
+imagine = mpimg.imread(first_image)
+axis[1].imshow(imagine)
+
+plt.show()
